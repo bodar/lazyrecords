751da8d0817a34cdf6aadf49799850c29b690144
dan <dan@bodar.com>
2012-08-03 12:04:24 +0100

    Cleanup to XmlRecords to make it automatically use root element and so definitions do not need to be changed

--- src/com/googlecode/lazyrecords/xml/mappings/XmlMapping.java
+++ src/com/googlecode/lazyrecords/xml/mappings/XmlMapping.java
@@ -1,13 +1,10 @@
 package com.googlecode.lazyrecords.xml.mappings;
 
 import com.googlecode.totallylazy.Sequence;
-import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
 public interface XmlMapping<T> {
-    Sequence<Node> to(Document document, String expression, T value) throws Exception;
+    Sequence<Node> to(Node node, String expression, T value) throws Exception;
 
     T from(Sequence<Node> nodes) throws Exception;
-
-
 }

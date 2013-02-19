6d2ff31e491ece6aa2affc5647714e383ae46d30
dan and phill <dan and phill@localhost>
2013-02-19 16:30:15 +0000

    Switched to using latest TL 1025, and started using Amazon CDN (repo.bodar.com)

--- test/com/googlecode/lazyrecords/KeywordsTest.java
+++ test/com/googlecode/lazyrecords/KeywordsTest.java
@@ -11,7 +11,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class KeywordsTest {
     @Test
-    @Ignore
     public void supportsToString() throws Exception {
         assertThat(where(keyword("Some column", Integer.class), Predicates.is(1)).toString(), is("where <Some column> is '1'"));
     }

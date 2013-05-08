00b7669c90d4f705654581e44b7deddda7f16670
rbarlow <rbarlow@raymanoz.com>
2013-05-08 23:03:18 +0100

    more tidy up

--- test/com/googlecode/lazyrecords/sql/SqlFunctionsTest.java
+++ test/com/googlecode/lazyrecords/sql/SqlFunctionsTest.java
@@ -30,7 +30,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class SqlFunctionsTest {
     private static Option<Connection> connection;
     private Logger logger = loggers(new MemoryLogger(), new PrintStreamLogger(new PrintStream(streams(System.out, new ByteArrayOutputStream()))));
-    ;
 
     @BeforeClass
     public static void setupOracle() {
@@ -60,29 +59,14 @@ public class SqlFunctionsTest {
         assertThat(sqlFunctions().get(ObjectIntegerFunction.class).length("Jorge"), is(5));
     }
 
-    @Test
-    public void supportsDateFunctions() {
-        assertThat(sqlFunctions().get(DateFunction.class).date("2003-12-31 01:02:03"), is(Dates.date(2003, 12, 31)));
+    public interface PrimativeIntegerFunction {
+        @SqlFunction("length")
+        int length(String value);
     }
 
-    @Test
-    public void supportsTimestampFunctions() {
-        assertThat(sqlFunctions().get(TimestampFunction.class).timestamp("2003-12-31 01:02:03"), is(new Timestamp(Dates.date(2003, 12, 31, 1, 2, 3).getTime())));
-    }
-
-    @Test
-    public void supportsBooleanFunctions() {
-        assertThat(sqlFunctions().get(BooleanFunction.class).isNull(null), is(true));
-    }
-
-    @Test
-    public void supportsBigDecimalFunctions() {
-        assertThat(sqlFunctions().get(BigDecimalFunction.class).sqrt(new BigDecimal("20")), is(new BigDecimal("4.47213595499958")));
-    }
-
-    @Test
-    public void supportsNumberFunctions() {
-        assertThat(sqlFunctions().get(NumberFunction.class).ceiling(new BigDecimal("3.14")), is((Number) new BigDecimal("4")));
+    public interface ObjectIntegerFunction {
+        @SqlFunction("length")
+        Integer length(String value);
     }
 
     @Test
@@ -91,9 +75,74 @@ public class SqlFunctionsTest {
         assertThat(sqlFunctions().get(ObjectLongFunction.class).length("Stuart"), is(new Long(6l)));
     }
 
+    public interface PrimativeLongFunction {
+        @SqlFunction("length")
+        long length(String value);
+    }
+
+    public interface ObjectLongFunction {
+        @SqlFunction("length")
+        Long length(String value);
+    }
+
+    @Test
+    public void supportsDateFunctions() {
+        assertThat(sqlFunctions().get(DateFunction.class).date("2003-12-31 01:02:03"), is(Dates.date(2003, 12, 31)));
+    }
+
+    public interface DateFunction {
+        @SqlFunction("date")
+        Date date(String value);
+    }
+
+    @Test
+    public void supportsTimestampFunctions() {
+        assertThat(sqlFunctions().get(TimestampFunction.class).timestamp("2003-12-31 01:02:03"), is(new Timestamp(Dates.date(2003, 12, 31, 1, 2, 3).getTime())));
+    }
+
+    public interface TimestampFunction {
+        @SqlFunction("timestamp")
+        Timestamp timestamp(String value);
+    }
+
+    @Test
+    public void supportsBooleanFunctions() {
+        assertThat(sqlFunctions().get(BooleanFunction.class).isNull(null), is(true));
+    }
+
+    public interface BooleanFunction {
+        @SqlFunction("isnull")
+        Boolean isNull(Object value);
+    }
+
+    @Test
+    public void supportsBigDecimalFunctions() {
+        assertThat(sqlFunctions().get(BigDecimalFunction.class).sqrt(new BigDecimal("20")), is(new BigDecimal("4.47213595499958")));
+    }
+
+    public interface BigDecimalFunction {
+        @SqlFunction("sqrt")
+        BigDecimal sqrt(BigDecimal value);
+    }
+
+    @Test
+    public void supportsNumberFunctions() {
+        assertThat(sqlFunctions().get(NumberFunction.class).ceiling(new BigDecimal("3.14")), is((Number) new BigDecimal("4")));
+    }
+
+    public interface NumberFunction {
+        @SqlFunction("ceiling")
+        Number ceiling(BigDecimal value);
+    }
+
     @Test
     public void supportsStringFunctions() {
         assertThat(sqlFunctions().get(StringFunction.class).lower("SHOUT!"), is("shout!"));
+    }
+
+    public interface StringFunction {
+        @SqlFunction("lower")
+        String lower(String value);
     }
 
     @Test
@@ -103,66 +152,7 @@ public class SqlFunctionsTest {
         assertThat(functions.upper("quiet!"), is("QUIET!"));
     }
 
-    @Test
-    public void supportsDefaultMethodName() {
-        assertThat(sqlFunctions().get(DefaultMethodNameFunctions.class).lower("ABC"), is("abc"));
-    }
-
-    private SqlFunctions sqlFunctions() {
-        return new SqlFunctions(connection.get(), new SqlMappings(), logger);
-    }
-
-    public static interface PrimativeIntegerFunction {
-        @SqlFunction("length")
-        int length(String value);
-    }
-
-    public static interface ObjectIntegerFunction {
-        @SqlFunction("length")
-        Integer length(String value);
-    }
-
-    public static interface PrimativeLongFunction {
-        @SqlFunction("length")
-        long length(String value);
-    }
-
-    public static interface ObjectLongFunction {
-        @SqlFunction("length")
-        Long length(String value);
-    }
-
-    public static interface DateFunction {
-        @SqlFunction("date")
-        Date date(String value);
-    }
-
-    public static interface TimestampFunction {
-        @SqlFunction("timestamp")
-        Timestamp timestamp(String value);
-    }
-
-    public static interface BooleanFunction {
-        @SqlFunction("isnull")
-        Boolean isNull(Object value);
-    }
-
-    public static interface BigDecimalFunction {
-        @SqlFunction("sqrt")
-        BigDecimal sqrt(BigDecimal value);
-    }
-
-    public static interface NumberFunction {
-        @SqlFunction("ceiling")
-        Number ceiling(BigDecimal value);
-    }
-
-    public static interface StringFunction {
-        @SqlFunction("lower")
-        String lower(String value);
-    }
-
-    public static interface ManyFunctions {
+    public interface ManyFunctions {
         @SqlFunction("lower")
         String lower(String value);
 
@@ -170,8 +160,17 @@ public class SqlFunctionsTest {
         String upper(String value);
     }
 
-    public static interface DefaultMethodNameFunctions {
+    @Test
+    public void supportsDefaultMethodName() {
+        assertThat(sqlFunctions().get(DefaultMethodNameFunctions.class).lower("ABC"), is("abc"));
+    }
+
+    public interface DefaultMethodNameFunctions {
         @SqlFunction
         String lower(String value);
+    }
+
+    private SqlFunctions sqlFunctions() {
+        return new SqlFunctions(connection.get(), new SqlMappings(), logger);
     }
 }
